6a2efa96c73636b4d170ee58660ec2a1530dd7f7
Nilesh <nilesh0627@gmail.com>
2021-07-05 15:32:57 +0530

    minor fix

--- client/src/molecules/CartBody/StyledCartBody.js
+++ client/src/molecules/CartBody/StyledCartBody.js
@@ -1,15 +1,11 @@
 import styled from "styled-components";
 
 export const StyledBody = styled.main`
-  height: 73vh;
+  /* height: 73vh; */
   max-height: calc(100vh - 18rem);
   overflow-y: scroll;
   background-color: var(--light-gray);
   padding-top: 1rem;
-
-  /* @media (max-width: 400px) {
-    height: 66vh;
-  } */
 `;
 
 export const StyledTagLine = styled.article`
@@ -18,6 +14,6 @@ export const StyledTagLine = styled.article`
   gap: 1rem;
   background-color: var(--white);
   padding: 1rem;
-  margin-bottom: 1rem;
+  margin-bottom: 5rem;
   font-size: 1.2rem;
 `;
